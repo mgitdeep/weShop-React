315538b5c892765e81161cefc69de6b516fc1ae9
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-02 16:44:39 +0530

    ex03

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 
-// input
-let userName = prompt("Enter your username: ")
+// // input
+// let userName = prompt("Enter your username: ")
 
-// processing
-let welcomeMsg = "This script works! " + userName;
+// // processing
+// let welcomeMsg = "This script works! " + userName;
 
-// output
-alert(welcomeMsg);
+// // output
+// alert(welcomeMsg);
+
+let btnTranslate = document.querySelector("#btn-translate")
+
+btnTranslate.addEventListener("click", function() {
+    console.log("clicked");
+})
