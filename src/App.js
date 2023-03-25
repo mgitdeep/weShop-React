6b4bffe04d26dc6a2d8c3fbb39bffa0fa380ae15
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-25 15:40:40 +0530

    Grouping Imports!
    - See the exports in "index.js" file for Pages & Components
    
    - See the pages & component Imports in "App.js"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Pages
+import {Home, Contact} from './pages'
 
+// Components
+import {Header, Footer} from './components'
 function App() {
   return (
     <>
-      <h1>Welcome all</h1>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
