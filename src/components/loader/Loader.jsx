86a1e9037513aed8a12c614955d4115453293650
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-26 17:45:38 +0530

    Made changes for the "loader"

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,17 @@
+
+import styles from './Loader.module.scss'
+import loaderImg from "../../assets/loader.png"
+
+const Loader = () => {
+  return ReactDOM.createPortal(
+    <div className={styles.wrapper}>
+        <div className={styles.loader}>
+            <img src={loaderImg} alt="loading..." />
+        </div>
+    </div>,
+
+    document.getElementById("loader")
+  )
+}
+
+export default Loader
