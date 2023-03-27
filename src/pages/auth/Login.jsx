abd8e4f7959779f276dc99798e237089703ae1b2
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-27 15:38:20 +0530

    Login page modifications

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,12 +1,68 @@
 import styles from './Auth.module.scss'
 import loginImg from "../../assets/login.png"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { BsGoogle } from 'react-icons/bs'
 import Card from '../../components/card/Card'
+import { useState } from 'react'
+import Loader from '../../components/loader/Loader'
+
+// Toastify imports
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Firebase integration
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../firebase/config'
 
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [loader, setLoader] = useState(false)
+
+  const navigateToLogin = useNavigate()
+
+  const handleLogin = (f) => {
+    f.preventDefault()
+    // console.log(email, password, confirmpass)
+    if ( email === "" || password === "") {
+      toast.error("Please enter details")
+    } else {
+
+      setLoader(true)
+      // Integrating with Firebase
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user)
+
+        setLoader(false)
+
+        toast.success("Login Successful!")
+        navigateToLogin("/login")
+     
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        // console.log(errorMessage.slice(10))
+        toast.error(errorMessage.slice(10))                 // we added this line :)
+
+        setLoader(false)
+      
+      });
+
+      setEmail("")
+      setPassword("")
+    }
+  }
+
   return (
+    <>
+    <ToastContainer />
+    { loader && <Loader />}
     <section className={` ${styles.auth}`}>
       <div className={styles.img}>
         <img src={loginImg} alt="login" width="400"/>
@@ -16,10 +72,15 @@ const Login = () => {
         <div className={styles.form}>
           <h2>Login</h2>
           <form>
-            <input type="text" placeholder='Email' required/>
-            <input type="password" placeholder='Password' required/>
+            <input type="text" placeholder='Email' required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
 
-            <button className='--btn --btn-primary --btn-block'>Login</button>
+            <input type="password" placeholder='Password' required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
+
+            <button className='--btn --btn-primary --btn-block' onClick={handleLogin}>Login</button>
 
             <div className={styles.links}>
               <Link to="/reset">Reset Password</Link>
@@ -35,6 +96,7 @@ const Login = () => {
       </Card>
       
     </section>
+    </>
   )
 }
 
