57ae26161ae6c53062a1f77966a309f58c2f32e8
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-27 12:58:14 +0530

    Fully functional Registration page w/out Firebase
    - See the beautiful logic here
    
    - Here we've addressed a user pain point. When password doesn't match all fields gets empty. Now the input field's data will stay there if password doesn't match so that user need not to write all again :smile:

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -2,34 +2,72 @@ import styles from './Auth.module.scss'
 import registerImg from "../../assets/register.png"
 import { Link } from 'react-router-dom'
 import Card from '../../components/card/Card'
+import { useState } from 'react'
+
+// Toastify imports
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmpass, setConfirmpass] = useState("")
+
+  const handleRegister = (f) => {
+    f.preventDefault()
+    // console.log(email, password, confirmpass)
+
+    if ( password !== confirmpass ) {
+      toast.error("Password do not match!")
+    } else {
+      toast.success("Successfully Registered!")
+
+      setEmail("")
+      setPassword("")
+      setConfirmpass("")
+    }
+
+    
+  }
+
   return (
-    <section className={` ${styles.auth}`}>
-      
-      <Card>
-        <div className={styles.form}>
-          <h2>Register</h2>
-          <form>
-            <input type="text" placeholder='Email' required/>
-            <input type="password" placeholder='Password' required/>
-            <input type="password" placeholder='Confirm Password' required/>
+    <>
+    <ToastContainer />
+      <section className={` ${styles.auth}`}>
+        
+        <Card>
+          <div className={styles.form}>
+            <h2>Register</h2>
+            <form>
+              <input type="text" placeholder='Email' required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}/>
 
-            <button className='--btn --btn-primary --btn-block'>Register</button>
+              <input type="password" placeholder='Password' required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
 
-          </form>
-          <span className={styles.register}>
-            <p>Already Registered?</p>
-            <Link to="/login">Login</Link>
-          </span>
+              <input type="password" placeholder='Confirm Password' required
+              value={confirmpass}
+              onChange={(e) => setConfirmpass(e.target.value)}/>
+
+              <button className='--btn --btn-primary --btn-block' onClick={handleRegister}>Register</button>
+
+            </form>
+            <span className={styles.register}>
+              <p>Already Registered?</p>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </Card>
+
+        <div className={styles.img}>
+          <img src={registerImg} alt="login" width="400"/>
         </div>
-      </Card>
-
-      <div className={styles.img}>
-        <img src={registerImg} alt="login" width="400"/>
-      </div>
-      
-    </section>
+        
+      </section>
+    </>
   )
 }
 
